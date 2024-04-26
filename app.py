5dd985af3f4f596711deb5c5eec78a711af52e67
Abhishek Shah <44420081+abh2050@users.noreply.github.com>
2024-04-25 21:31:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ for example_url in example_urls:
             st.success('The URL is likely safe.')
         else:
             st.error('The URL is potentially phishing.')
-        st.write('Phishing Probability:', loaded_model.predict_proba(vectorized_url)[0][1])
+        st.write('Not Phishing Probability:', loaded_model.predict_proba(vectorized_url)[0][1])
